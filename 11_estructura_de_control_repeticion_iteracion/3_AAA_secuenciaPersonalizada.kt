77a215bd4ee0a92e3bfd15f350d91fb1ee078283
nodos-academy <josias.creus@nodosacademy.com>
2025-04-30 12:27:11 -0600

    se agregó comentario

---
+++ 11_estructura_de_control_repeticion_iteracion/3_AAA_secuenciaPersonalizada.kt
@@ -0,0 +1,23 @@
+// Algoritmo que solicita al usuario un número de inicio y un número tope
+// Luego, usa un ciclo mientras para mostrar los valores desde el número inicial hasta el tope (inclusive)
+
+fun main() {
+    println("Ingresa un número como tope para este ciclo:")
+    val tope = readLine().toString().toInt()
+
+    println("Ingresa un número como valor incial:")
+    var contador = readLine().toString().toInt()
+
+    println("\nValor del tope: $tope")
+    println("Valor inicial del contador: $contador\n")
+
+    if (contador <= tope) {
+        while (contador <= tope) {
+            println("La variable contiene el dato: $contador")
+            contador = contador + 1
+        }
+    }
+    else {
+        println("El tope debe ser mayor o igual que el contador...")
+    }
+}
---
+++ 11_estructura_de_control_repeticion_iteracion/4_decremento.kt
@@ -0,0 +1,12 @@
+// Algoritmo que utiliza un ciclo mientras para contar en reversa desde 5 hasta 1
+// En cada iteración, imprime el valor actual y lo decrementa en uno
+
+fun main() {
+
+    var variable = 5.0
+
+    while (variable > 0) {
+        println("La variable contiene el dato: $variable")
+        variable-- //decremento:  variable = variable - 1
+    }
+}
